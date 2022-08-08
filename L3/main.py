2b1ea44b9ccec8dc6bd9879ac1d9e4e57435e6a8
Volodymyr <110393450+VolodimirMuliarMih@users.noreply.github.com>
2022-08-08 23:11:24 +0300

    Update main.py
    удалил стр

--- L3/main.py
+++ L3/main.py
@@ -1,7 +1,5 @@
 # leasso3
 name1, name2 = list(input("Введите текс из двух слов:").split())
-name1 = str(name1)
-name2 = str(name2)
 name3 = "{1} {0}".format(name1,name2)
 name4 = "{1} {0}".format(name1.upper(),name2.title())
 name5 = f"!{name1} {name2}?"
